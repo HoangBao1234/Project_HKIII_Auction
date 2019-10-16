d053a14d8518470f05bd44c360527ecaa30654a8
nhoxnhanpham123 <56584780+nhoxnhanpham123@users.noreply.github.com>
2019-10-16 09:12:30 +0700

    hihi

---
+++ Project_HKIII_Auction/Project_HKIII_Auction/Controllers/AutionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_HKIII_Auction.Controllers
+{
+    public class AutionController : Controller
+    {
+        // GET: Aution
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
