bdecc095966368b4576f12e0de1da95a27afac89
Nguyen Hoang Bao <baonguyen5910@gmail.com>
2019-10-18 20:36:09 +0700

    Admin

--- Project_HKIII_Auction/Project_HKIII_Auction/Controllers/AdminController.cs
+++ Project_HKIII_Auction/Project_HKIII_Auction/Controllers/AdminController.cs
@@ -15,9 +15,11 @@ namespace Project_HKIII_Auction.Controllers
         ProductDal PDal = new ProductDal();
         UserDal UDal = new UserDal();
         CategoryDal CDal = new CategoryDal();
+        HistoryDal HDal = new HistoryDal();
+        Context context = new Context();
         public ActionResult Index()
         {
-            ViewBag.ACount = Adal.GetAdmins().Count();
+            ViewBag.CCount = CDal.GetCategories().Count();
             ViewBag.PCount = PDal.GetProducts().Count();
             ViewBag.UCount = UDal.GetUsers().Count();
 
@@ -63,6 +65,76 @@ namespace Project_HKIII_Auction.Controllers
         public ActionResult Products()
         {
             return View(PDal.GetProducts());
+        }
+        [HttpPost]
+        public ActionResult SreachUs(string txtSearch)
+        {
+            if (string.IsNullOrEmpty(txtSearch))
+            {
+                return View("Users", UDal.GetUsers()); ;
+            }
+            else
+            {
+                var list = UDal.GetUsers().Where(e => e.UName.ToLower().Contains(txtSearch) || e.UName.ToUpper().Contains(txtSearch));
+                return View("Users", list.ToList());
+            }
+        }
+        [HttpPost]
+        public ActionResult SreachPro(string txtSearch)
+        {
+            if (string.IsNullOrEmpty(txtSearch))
+            {
+                return View("Products", PDal.GetProducts());
+            }
+            else
+            {
+                var list = PDal.GetProducts().Where(e=>e.PName.ToLower().Contains(txtSearch) || e.PName.ToUpper().Contains(txtSearch));
+                return View("Products", list.ToList());
+            }
+        }
+        [HttpPost]
+        public ActionResult SearchCate(string txtSearch)
+        {
+            if (string.IsNullOrEmpty(txtSearch))
+            {
+                return View("categories", CDal.GetCategories());
+            }
+            else
+            {
+                var list = CDal.GetCategories().Where(e => e.CName.ToUpper().Contains(txtSearch) || e.CName.ToLower().Contains(txtSearch));
+                return View("Categories", list.ToList());
+            }
+        }
+
+        public ActionResult HistoryAuction()
+        {
+            var His = HDal.GetHistoryAuctions();
+            var Pros = PDal.GetProducts();
+            var Users = UDal.GetUsers();
+
+            var List = from U in Users join H in His on U.UId equals H.UId join P in Pros on H.PId equals P.PId select new {U.UName, P.PName, P.MinimumPrice, H.PriceAuction, H.Status };
+            var model = (object)JsonConvert.SerializeObject(List);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult SearchHist(string txtSearch)
+        {
+            var His = HDal.GetHistoryAuctions();
+            var Pros = PDal.GetProducts();
+            var Users = UDal.GetUsers();
+
+            var List = from U in Users join H in His on U.UId equals H.UId join P in Pros on H.PId equals P.PId select new { U.UName, P.PName, P.MinimumPrice, H.PriceAuction, H.Status };
+            var model = (object)JsonConvert.SerializeObject(List);
+            if (string.IsNullOrEmpty(txtSearch))
+            {
+                return View("HistoryAuction", model);
+            }
+            else
+            {
+                var searched = List.Where(e=>e.UName.ToLower().Contains(txtSearch) || e.UName.ToUpper().Contains(txtSearch));
+                var model1 = (object)JsonConvert.SerializeObject(searched);
+                return View("HistoryAuction", model1);
+            }
         }
     }
 }
---
+++ Project_HKIII_Auction/Project_HKIII_Auction/Models/HistoryDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_HKIII_Auction.Models
+{
+    public class HistoryDal
+    {
+        Context context = new Context();
+        public List<HistoryAuction> GetHistoryAuctions()
+        {
+            return context.HistoryAuctions.ToList();
+        }
+    }
+}
