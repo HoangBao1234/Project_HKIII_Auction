7ffbe498d85850bb5434dee1b628b19bffccc2c3
Nguyen Hoang Bao <baonguyen5910@gmail.com>
2019-10-23 19:55:07 +0700

    login

--- Project_HKIII_Auction/Project_HKIII_Auction/Controllers/SignInController.cs
+++ Project_HKIII_Auction/Project_HKIII_Auction/Controllers/SignInController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_HKIII_Auction.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,23 @@ namespace Project_HKIII_Auction.Controllers
     public class SignInController : Controller
     {
         // GET: SignIn
+        UserDal dal = new UserDal();
         public ActionResult Index_Signin()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Check_Signin(User user)
+        {
+            if (dal.Create(user))
+            {
+                return RedirectToAction("Index", "User");
+            }
+            else
+            {
+                ViewBag.Msg = "This user name has already existed. Please recreate another user name";
+                return View("Index_Signin");
+            }
         }
     }
 }
