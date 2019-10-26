d9fec87080e0ac2cd1c289d4b13f40d32f12f1f1
Nguyen Hoang Bao <baonguyen5910@gmail.com>
2019-10-26 09:43:24 +0700

    Upload

--- Project_HKIII_Auction/Project_HKIII_Auction/Controllers/UserController.cs
+++ Project_HKIII_Auction/Project_HKIII_Auction/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_HKIII_Auction.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace Project_HKIII_Auction.Controllers
     public class UserController : Controller
     {
         // GET: User
+        ProductDal PDal = new ProductDal();
         public ActionResult Index()
         {
             return View();
@@ -24,6 +26,30 @@ namespace Project_HKIII_Auction.Controllers
         public ActionResult Tivi()
         {
             return View();
+        }
+        public ActionResult CreateProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                if (PDal.Create(product))
+                {
+                    ModelState.Clear();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
