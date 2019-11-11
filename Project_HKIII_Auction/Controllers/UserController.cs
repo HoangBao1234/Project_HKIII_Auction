1ed6e3dbb799ffe2b571df42e33aaa3e2fb352a2
Nguyen Hoang Bao <baonguyen5910@gmail.com>
2019-11-11 10:11:51 +0700

    a

--- Project_HKIII_Auction/Controllers/UserController.cs
+++ Project_HKIII_Auction/Controllers/UserController.cs
@@ -13,25 +13,16 @@ namespace Project_HKIII_Auction.Controllers
     public class UserController : Controller
     {
         // GET: User
-        AdminDal Adal = new AdminDal();
         ProductDal PDal = new ProductDal();
         UserDal UDal = new UserDal();
         CategoryDal CDal = new CategoryDal();
         HistoryDal HDal = new HistoryDal();
         Context context = new Context();
-        public ActionResult Index()
-        {
-            return View();
-        }
         public ActionResult AboutUs()
         {
             return View();
         }
         public ActionResult ContactUs()
-        {
-            return View();
-        }
-        public ActionResult Tivi()
         {
             return View();
         }
@@ -114,12 +105,26 @@ namespace Project_HKIII_Auction.Controllers
         [HttpPost]
         public ActionResult Auction(int PId, int Price)
         {
+            var user = (Project_HKIII_Auction.Common.UserLogin)Session["userSession"];
             string sCon = ConfigurationManager.ConnectionStrings["hihi"].ConnectionString;
             SqlConnection conn = new SqlConnection(sCon);
-            string query = "Update Products Set Incremenent = "+Price+" Where PId = "+PId;
+            string query = "Update Products Set Incremenent = @Price Where PId = @PId";
+            string queryHis = "Insert into HistoryAuctions Values(@PId, @UId, @Price, @Status)";
             conn.Open();
+            //Update Price Auction
             SqlCommand command = new SqlCommand(query, conn);
+            command.Parameters.Add(new SqlParameter("Price", Price));
+            command.Parameters.Add(new SqlParameter("PId", PId));
+            //Add History Auction
+            SqlCommand command1 = new SqlCommand(queryHis, conn);
+            command1.Parameters.Add(new SqlParameter("PId", PId));
+            command1.Parameters.Add(new SqlParameter("UId", user.UId));
+            command1.Parameters.Add(new SqlParameter("Price", Price));
+            command1.Parameters.Add(new SqlParameter("Status", "Waiting"));
+            command1.ExecuteNonQuery();
+            
             int i = command.ExecuteNonQuery();
+            conn.Close();
             if (i > 0)
             {
                 return RedirectToAction("Home");
@@ -128,7 +133,19 @@ namespace Project_HKIII_Auction.Controllers
             {
                 return View();
             }
-            
+        }
+        public ActionResult MyProduct(string UName)
+        {
+            var P = PDal.GetProducts();
+            var C = CDal.GetCategories();
+            var U = UDal.GetUsers();
+
+            var list = from u in U join p in P on u.UId equals p.UId join c in C on p.CId equals c.CId select new { u.UName, p.PName, p.Image, p.MinimumPrice, c.CName, p.DateStart, p.DateEnd, p.PId, p.Incremenent, p.Status };
+            var MyList = list.Where(e => e.UName.Equals(UName));
+
+            var MyProduct = (object)JsonConvert.SerializeObject(MyList);
+
+            return View(MyProduct);
         }
     }
 }
